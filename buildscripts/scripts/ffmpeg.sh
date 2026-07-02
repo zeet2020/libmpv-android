af072a35d7dd10dfe6fbbd8ffe79547be28fcc4d
zeeshan ahmed <zeet2020@gmail.com>
2026-07-02 20:28:00 +0530

    Update ffmpeg.sh
    enable visualizer

--- buildscripts/scripts/ffmpeg.sh
+++ buildscripts/scripts/ffmpeg.sh
@@ -33,6 +33,7 @@ cpuflags=
 	--disable-{muxers,encoders,devices,filters} \
 	--enable-filter=loudnorm \
 	--enable-filter=equalizer \
+	--enable-filter=showcqt,showspectrum,showwaves,avectorscope,showvolume,format,aformat,asplit \
 	--disable-v4l2-m2m
 
 make -j$cores
